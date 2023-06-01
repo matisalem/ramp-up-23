a552900dad4909fee44b9e1ba17c829401a25635
Matias Salem <matiassalem17@gmail.com>
2023-06-01 13:17:19 -0300

    oddEven fixed

--- practice/oddeven.py
+++ practice/oddeven.py
@@ -1,9 +1,9 @@
 def main():
     number = int(input("number: "))
     if number % 2 == 0:
-        print(f"{number} is even")
+        print(number, " is even")
     else:
-        print(f"{number} is odd")
+        print(number,  "is odd")
 
 if __name__ == "__main__":
     main()
